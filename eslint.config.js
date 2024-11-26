7df6b2c862395c36a0f990c42852de979573b495
Fernando Isidro Luna <luffynando@gmail.com>
2024-11-26 00:53:04 -0600

    style: :arrow_up: update dependencies and update eslint config for latest changes

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,4 @@ import nodecfdiConfig from '@nodecfdi/eslint-config';
 
 const { defineConfig } = nodecfdiConfig(import.meta.dirname, { adonisjs: true, sonarjs: true, n: true });
 
-export default defineConfig({
-  files: ['**/providers/**/*.ts'],
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': 'off',
-  },
-});
+export default defineConfig();
