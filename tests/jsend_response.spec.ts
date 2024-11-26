909cac4fbeccb89a771fa267f865b323642be220
Fernando IL <luffynando@gmail.com>
2024-11-26 11:25:17 -0600

    test: :test_tube: add tests for jsend response

---
+++ tests/jsend_response.spec.ts
@@ -0,0 +1,49 @@
+import { HttpContextFactory } from '@adonisjs/core/factories/http';
+import { ResponseStatus } from '@adonisjs/core/http';
+import { test } from '@japa/runner';
+
+test.group('Jsend response', (group) => {
+  group.each.setup(async () => {
+    await import('../src/jsend_response_macros.js');
+  });
+
+  test('jsend success generates a response with status code 200', ({ assert }) => {
+    const ctx = new HttpContextFactory().create();
+    ctx.response.jsendSuccess();
+
+    assert.equal(ctx.response.getStatus(), ResponseStatus.Ok);
+    assert.isObject(ctx.response.getBody());
+    assert.properties(ctx.response.getBody(), ['status']);
+    assert.containsSubset(ctx.response.getBody(), { status: 'success' });
+  });
+
+  test('jsend success generates a response with specific status code', ({ assert }) => {
+    const ctx = new HttpContextFactory().create();
+    ctx.response.jsendSuccess('hi', ResponseStatus.Created);
+
+    assert.equal(ctx.response.getStatus(), ResponseStatus.Created);
+    assert.isObject(ctx.response.getBody());
+    assert.properties(ctx.response.getBody(), ['status', 'data']);
+    assert.containsSubset(ctx.response.getBody(), { status: 'success', data: 'hi' });
+  });
+
+  test('jsend error generates a response with status code 500', ({ assert }) => {
+    const ctx = new HttpContextFactory().create();
+    ctx.response.jsendError('Something happened');
+
+    assert.equal(ctx.response.getStatus(), ResponseStatus.InternalServerError);
+    assert.isObject(ctx.response.getBody());
+    assert.properties(ctx.response.getBody(), ['status', 'message']);
+    assert.containsSubset(ctx.response.getBody(), { status: 'error', message: 'Something happened' });
+  });
+
+  test('jsend fail generates a response with status code 400', ({ assert }) => {
+    const ctx = new HttpContextFactory().create();
+    ctx.response.jsendFail({ message: 'Something happened' });
+
+    assert.equal(ctx.response.getStatus(), ResponseStatus.BadRequest);
+    assert.isObject(ctx.response.getBody());
+    assert.properties(ctx.response.getBody(), ['status', 'data']);
+    assert.containsSubset(ctx.response.getBody(), { status: 'fail', data: { message: 'Something happened' } });
+  });
+});
